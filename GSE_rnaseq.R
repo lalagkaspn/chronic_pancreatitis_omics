dbb5e9df1f66e12d63f2c952b5de61a5558eaad0
lalagkaspn <panagiotis.lalagkas@gmail.com>
2023-10-14 12:10:12 -0400

    RNA-seq raw counts to TPM

--- GSE_rnaseq.R
+++ GSE_rnaseq.R
@@ -37,7 +37,6 @@ for(i in 1:length(GEOsets)) {
 }
 names(pdata) = names(GEOsets); rm(i)
 
-
 ### Keep only necessary pdata
 filt_pdata = list()
 
@@ -125,25 +124,28 @@ filt_pdata$GSE133684$Tissue_type = factor(x = filt_pdata$GSE133684$Tissue_type,
 
 ## GSE194331: convert raw counts to TPM
 
-# Create a TPM matrix
-library(AnnotationHub)
+# Summary of the counts table
+summary(GSE194331_raw_counts[, 2:5]) # --> needs normalization --> will calculate TPM
 
+## Calculate TPM:
+## 1. Calculate RPK (divide counts with transcript lengths in kB) --> reads per kilobase
+## 2. Find scaling factor for each sample by summing all the RPK of each sample and divide by 1,000,000
+## 3. Multiply each value within each sample with that scaling factor --> transcript per million (TPM)
+
+# Add transcript length
+library(AnnotationHub)
 # Create an AnnotationHub object
 ah = AnnotationHub()
-
 # Query for the latest Homo sapiens EnsDb object
 latest_homo_sapiens = query(ah, c("EnsDb", "Homo sapiens")) 
-
 # Print the latest version
 tail(latest_homo_sapiens)
-
 # ID of interest is "AH113665"
 edb = ah[["AH113665"]]
 
 # Get gene data
 genes_data = genes(edb)
 entrezid_temp = genes_data$entrezid
-
 genes_map = data.frame(gene_id = NA, entrezID = NA)
 for (i in 1:length(entrezid_temp)) {
   genes_map[i, "gene_id"] = names(entrezid_temp)[i]
@@ -161,10 +163,7 @@ transcript_lengths = data.frame(transcript_id = transcripts_data$tx_id,
   group_by(entrezID) %>%
   summarise(longest_transcript_length = max(tx_length, na.rm = TRUE))
 
-# Calculate gene lengths in kilobases
-transcript_lengths$transcript_length_kb = transcript_lengths$longest_transcript_length / 1000
-
-# Each row may contain multiple entrezIDs --> split them into separate rows --> find the longest transcript length for duplicated entrezIDs
+# Each row may contain multiple entrezIDs --> split them into separate rows --> keep the longest transcript length for duplicated entrezIDs
 temp = stringr::str_split_fixed(transcript_lengths$entrezID, ",", n = Inf)
 transcript_lengths = cbind(transcript_lengths[, 2], temp)
 transcript_lengths = reshape2::melt(transcript_lengths, "longest_transcript_length", colnames(transcript_lengths)[2:ncol(transcript_lengths)])
@@ -183,34 +182,18 @@ transcript_lengths_in_raw_counts = transcript_lengths_in_raw_counts %>% arrange(
 GSE194331_raw_counts = GSE194331_raw_counts %>% dplyr::filter(GeneID %in% transcript_lengths_in_raw_counts$entrezID)
 GSE194331_raw_counts = GSE194331_raw_counts %>% dplyr::arrange(GeneID)
 
-# Calculate counts per kilobase (CPK)
-GSE194331_cpk = data.frame(GSE194331_raw_counts)
-for (i in 1:nrow(GSE194331_cpk)) {
-  gene_temp = GSE194331_cpk[i, "GeneID"]
-  transcript_length = transcript_lengths_in_raw_counts %>% dplyr::filter(entrezID == gene_temp)
-  GSE194331_cpk[i, 2:ncol(GSE194331_cpk)] = GSE194331_cpk[i, 2:ncol(GSE194331_cpk)] / transcript_length$longest_transcript_length
-  cat(i, "\n")
-} ; rm(i, gene_temp, transcript_length)
-# Calculate the sum of CPK values for each sample
-GSE194331_cpk_sum = colSums(GSE194331_cpk)
+sum(transcript_lengths_in_raw_counts$entrezID == GSE194331_raw_counts$GeneID) == nrow(GSE194331_raw_counts) # TRUE --> entrezIDs are in the same order in both files
 
-# Calculate TPM values
-GSE194331_tpm = GSE194331_cpk / GSE194331_cpk_sum * 1e6
-GSE194331_tpm$GeneID = GSE194331_raw_counts$GeneID
+# Calculate RPK
+# Divide the counts by transcript length in kB
+GSE194331_rpk = apply(subset(GSE194331_raw_counts, select = c(-GeneID)), 2,
+                      function(x) x / (transcript_lengths_in_raw_counts$longest_transcript_length / 1000))
+GSE194331_rpk = as.data.frame(GSE194331_rpk)
 
-rm(GSE194331_cpk, GSE194331_cpk_sum,  ah, edb, temp, transcript_lengths, transcript_lengths_in_raw_counts, transcript_length_kb, transcripts_data, genes_data, genes_map, entrezid_temp)
+# Calculate TPM
+GSE194331_tpm = apply(GSE194331_rpk, 2,
+                      function (x) x / sum(as.numeric(x)) * 10^6)
+GSE194331_tpm = as.data.frame(GSE194331_tpm)
+GSE194331_tpm$GeneID = GSE194331_raw_counts$GeneID ; GSE194331_tpm = GSE194331_tpm %>% dplyr::relocate(GeneID, .before = GSM5833563)
 
-## GSE133684_tpm: convert ensembl gene IDs to Entrez Gene IDs
-
-## RefSeq to EntrezID reference data frame
-ref = org.Hs.egENSEMBL2EG
-mapped_genes_official = mappedkeys(ref)
-ref_df = as.data.frame(ref[mapped_genes_official]) ; rm(ref, mapped_genes_official)
-ref_df = ref_df %>% dplyr::rename(ENTREZ_GENE_ID = gene_id)
-length(unique(ref_df$ensembl_id)) == length(ref_df$ensembl_id) # FALSE --> duplicates in RefSeq --> remove them
-# remove ensembl_ids that match to more than one entrezID
-duplicated_ensembl = unique(ref_df[which(duplicated(ref_df$ensembl_id)), "ensembl_id"])
-ref_df = ref_df %>% filter(!ensembl_id %in% duplicated_ensembl) ; rm(duplicated_ensembl)
-GSE133684_tpm = left_join(GSE133684_tpm, ref_df, by = c("V1" = "ensembl_id"))
-GSE133684_tpm = GSE133684_tpm %>% dplyr::relocate(ENTREZ_GENE_ID, .before = V1)
-
+rm(GSE194331_rpk, ah, edb, temp, transcript_lengths, transcript_lengths_in_raw_counts, transcripts_data, genes_data, genes_map, entrezid_temp, latest_homo_sapiens)
